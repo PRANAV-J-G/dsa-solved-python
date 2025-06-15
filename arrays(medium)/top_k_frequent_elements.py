aa44d9e654fbad4e96686eb9f705291ab3db0af7
PRANAV <pranavjg03@gmail.com>
2025-06-15 21:46:18 +0530

    15/6/25

---
+++ arrays(medium)/top_k_frequent_elements.py
@@ -0,0 +1,73 @@
+# own solution (may not be optimized)
+
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        res = defaultdict(int)
+
+        for num in nums:
+            if num in res:
+                res[num] += 1
+            else:
+                res[num] = 1
+
+        sorted_dict = dict(sorted(res.items(),key=lambda item:item[1],reverse = True))
+
+        return list(sorted_dict.keys())[:k]
+    
+# Bucket sort 
+
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+                
+# Min heap
+from collections import heapq
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+                
+# sorting approach 
+
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        arr = []
+        for num, cnt in count.items():
+            arr.append([cnt, num])
+        arr.sort()
+
+        res = []
+        while len(res) < k:
+            res.append(arr.pop()[1])
+        return res
+
